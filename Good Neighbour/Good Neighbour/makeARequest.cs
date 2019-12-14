f8901b987814193dfd5e37935ea4a52ba1bb20cc
Julie <46454827+juliengo141@users.noreply.github.com>
2019-12-14 07:52:53 -0500

    Update makeARequest.cs

--- Good Neighbour/Good Neighbour/makeARequest.cs
+++ Good Neighbour/Good Neighbour/makeARequest.cs
@@ -16,5 +16,12 @@ namespace Good_Neighbour
         {
             InitializeComponent();
         }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GoodNeighbourForm myForm = new GoodNeighbourForm();
+            this.Hide();
+            myForm.ShowDialog();
+            this.Close();
+        }
     }
 }
